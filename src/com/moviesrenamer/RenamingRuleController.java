2933e3cd28449f696c852294df0a5244e3ead426
Alberto Malagoli <albemala@gmail.com>
2016-07-10 22:04:20 +0100

    set some actions on buttons

--- src/com/moviesrenamer/RenamingRuleController.java
+++ src/com/moviesrenamer/RenamingRuleController.java
@@ -1,5 +1,7 @@
 package com.moviesrenamer;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
@@ -12,12 +14,15 @@ public class RenamingRuleController {
     @FXML
     private ListView<String> renamingRuleListView;
 
+    private ObservableList<String> renamingRule;
+
     public RenamingRuleController() {
+        renamingRule = FXCollections.observableArrayList();
     }
 
     @FXML
     public void initialize() {
-
+        renamingRuleListView.setItems(renamingRule);
     }
 
     @FXML
@@ -27,35 +32,44 @@ public class RenamingRuleController {
 
     @FXML
     public void clearRule() {
-
+        renamingRule.clear();
     }
 
     @FXML
     public void addTitle() {
+        renamingRule.add("title");
     }
 
     @FXML
     public void addOriginalTitle() {
+        renamingRule.add("originalTitle");
     }
 
     @FXML
     public void addYear() {
+        renamingRule.add("year");
     }
 
     @FXML
     public void addRuntime() {
+        renamingRule.add("runtime");
     }
 
     @FXML
     public void addLanguage() {
+        renamingRule.add("language");
     }
 
     @FXML
     public void addRoundBrackets() {
+        renamingRule.add("(");
+        renamingRule.add(")");
     }
 
     @FXML
     public void addSquareBrackets() {
+        renamingRule.add("[");
+        renamingRule.add("]");
     }
 
     @FXML
